54efd46227ad106e76ca954591d4a059c049bb7b
OGDR <eec58impal@tutanota.com>
2026-07-30 09:28:07 -0700

    fix path because of where mk file was placed. was doing a double path

--- motorola/kansas/recovery/root/Android.mk
+++ motorola/kansas/recovery/root/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),kansas)
 
-$(shell cp -a $(LOCAL_PATH)/recovery/root/. $(TARGET_RECOVERY_ROOT_OUT)/)
+$(shell cp -a $(LOCAL_PATH)/. $(TARGET_RECOVERY_ROOT_OUT)/)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
